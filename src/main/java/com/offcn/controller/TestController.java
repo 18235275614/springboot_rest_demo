19644ddd391b9c74ffe6e869f8cc1c869797cfdb
java-cxh <2835920117@qq.com>
2020-10-27 23:03:26 +0800

    测试分支版本1.0文件

--- src/main/java/com/offcn/controller/TestController.java
+++ src/main/java/com/offcn/controller/TestController.java
@@ -4,6 +4,10 @@ public class TestController {
 
     public String test(){
         System.out.println("嘿嘿!");
+        //add by cxh
+        while (true){
+            return "";
+        }
         return null;
     }
 }
